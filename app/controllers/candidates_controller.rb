82947931cd8a80d98fcf199103f0c94c42e0e680
Andre Wu <secret8419@gmail.com>
2017-11-24 09:47:10 +0800

    1124_candidateWeb

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,7 +1,22 @@
 class CandidatesController < ApplicationController
   def index
+    @candidate = Candidate.all
   end
 
   def new
+    @candidate = Candidate.new
   end
+
+  def create
+    # Strong Parameters(require抓取candidate有關的，name party age欄位)
+    clean_params = params.require(:candidate).permit(:name, :party, :age)
+    candidate = Candidate.new(clean_params)
+
+    if candidate.save
+      redirect_to candidates_path
+    else
+      #...
+    end
+  end
+
 end
