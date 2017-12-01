5a521f00e41e4bb46eff48309ffc77ebc547de49
Andre Wu <secret8419@gmail.com>
2017-12-01 11:40:58 +0800

    add migration

---
+++ db/migrate/20171201033700_add_comment_to_candidate.rb
@@ -0,0 +1,5 @@
+class AddCommentToCandidate < ActiveRecord::Migration[5.1]
+  def change
+    add_column :candidates, :comment, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117024945) do
+ActiveRecord::Schema.define(version: 20171201033700) do
 
   create_table "candidates", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171117024945) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "comment"
   end
 
 end
